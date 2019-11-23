3fdae9864d06d004bfa93bea8eaf69f8a4090a95
EgorCh38 <egorch3802@mail.ru>
2019-11-23 10:57:11 +0200

    init

---
+++ ding.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int fib(n)
+{
+	return ( n ==  1 || n == 2) ? 1 : fib( n - 1 ) + fib( n - 2 );
+}
+
+int main()
+{
+	int a,ans;
+	while(scanf("%d",&a) != EOF)
+	{
+		ans = fib(a);
+		printf("%d ",  ans);
+	}
+	return 0;
+}
